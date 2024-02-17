08c4a034fd47f7215277ba555fe05435cada55a7
tiroumalaifreddy <36243250+tiroumalaifreddy@users.noreply.github.com>
2024-02-17 22:49:34 +0100

    Gog api (#2)
    * problem OK
    
    * access token ok
    
    * ok to get ids of owned games
    
    * get name and if of goggame

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use dotenv::dotenv;
 use reqwest;
 use steam::steamgames::SteamGame;
+use thirtyfour::session::http;
 use std::env;
 use serde_json;
 use std::fs::File;
@@ -9,48 +10,19 @@ use std::io::BufWriter;
 use duckdb::{params, Connection, Result};
 
 mod steam;
+mod gog;
 
 mod games;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    dotenv().ok();
-
-    let steam_api_key: String = env::var("STEAM_API_KEY").expect("Missing an API key");
-    let steamid: u64 = 76561198118055178;
-
-    let http_client: reqwest::Client = reqwest::Client::new();
-
-    let result: Result<Vec<SteamGame>, reqwest::Error> = steam::steamgames::get_owned_games(http_client, steam_api_key, steamid).await;
-
-    let conn: Connection = Connection::open("temp/test.db")?;
-
-    conn.execute_batch(
-        r"CREATE SEQUENCE seq;
-          CREATE TABLE IF NOT EXISTS game (
-                  appid              INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
-                  name            TEXT NOT NULL,
-                  playtime INTEGER,
-                  platform TEXT NOT NULL
-                  );
-        ")?;
+    let toktok = gog::test::get_token().await?;
     
-    let unwraped_result: Vec<SteamGame> = result.unwrap();
-
-    let games_format: games::Games = games::Games::from_steam_games(unwraped_result);
-                  
-
-
-
-    for element  in games_format.games {
-        conn.execute(
-            "INSERT INTO game (appid, name, playtime, platform) VALUES (?, ?, ?, ?)",
-            params![element.appid, element.name, element.playtime, element.platform],
-        )?;
-    }
-    let _ = conn.close();
-
+    let http_client: reqwest::Client = reqwest::Client::new();
+    let games_id = gog::test::get_owned_games_ids(&http_client, &toktok);
+    let owned_games_id: Vec<serde_json::Value> = games_id.await?;
+    let games = gog::test::get_owned_games(&http_client, &toktok, owned_games_id);
+    println!("{:?}", games.await?);
     Ok(())
 
 }
