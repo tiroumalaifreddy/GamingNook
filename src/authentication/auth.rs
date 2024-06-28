474175dcaa4954a3d3cd4b9208f79f4c00dea67d
tiroumalaifreddy <ftiroumala@gmail.com>
2024-06-28 16:40:04 +0200

    error building

---
+++ src/authentication/auth.rs
@@ -0,0 +1,127 @@
+use bcrypt::{hash, verify, DEFAULT_COST};
+use rusqlite::{params, Connection};
+use serde::{Deserialize, Serialize};
+use actix_session::{storage::RedisSessionStore, Session, SessionMiddleware};
+use actix_web::{
+    cookie::{Key, SameSite},
+    error::InternalError,
+    middleware, web, App, Error, HttpResponse, HttpServer, Responder,
+};
+
+#[derive(Deserialize)]
+pub struct Credentials {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: i64,
+    username: String,
+    password: String,
+    steamid: String,
+    gogid: String,
+    epicid: String
+}
+
+impl User {
+    fn authenticate(credentials: Credentials) -> Result<Self, HttpResponse> {
+        let conn = Connection::open("temp/test.db3").unwrap();
+        let mut stmt = conn.prepare("SELECT id, password FROM users WHERE username = ?1").unwrap();
+        let mut user_rows = stmt.query(params![credentials.username]).unwrap();
+
+        if let Some(row) = user_rows.next().unwrap() {
+            let id: i64 = row.get(0).unwrap();
+            let db_password: String = row.get(1).unwrap();
+            let steam_id: String = row.get(2).unwrap();
+            let gog_id: String = row.get(3).unwrap();
+            let epic_id: String = row.get(4).unwrap();
+
+            if verify(&credentials.password, &db_password).unwrap() {
+                return Ok(User {
+                    id,
+                    username: credentials.username,
+                    password: db_password,
+                    steamid: steam_id,
+                    gogid: gog_id,
+                    epicid: epic_id
+                });
+            }
+        }
+
+        Err(HttpResponse::Unauthorized().json("Unauthorized"))
+    }
+
+    fn create(credentials: Credentials) -> Result<Self, HttpResponse> {
+        let conn = Connection::open("temp/test.db3").unwrap();
+
+        conn.execute_batch(
+            r"CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+                steamid TEXT,
+                gogid TEXT,
+                epicid TEXT
+            );"
+        ).unwrap();
+
+        let hashed_password = hash(&credentials.password, DEFAULT_COST).unwrap();
+        conn.execute(
+            "INSERT INTO users (username, password) VALUES (?1, ?2)",
+            params![credentials.username, hashed_password],
+        ).unwrap();
+
+        let id = conn.last_insert_rowid();
+
+        Ok(User {
+            id,
+            username: credentials.username,
+            password: hashed_password,
+            steamid: String::new(),
+            gogid: String::new(),
+            epicid: String::new()
+        })
+    }
+}
+
+pub async fn register(credentials: web::Json<Credentials>) -> Result<impl Responder, Error> {
+    let credentials = credentials.into_inner();
+
+    match User::create(credentials) {
+        Ok(user) => Ok(HttpResponse::Ok().json(user)),
+        Err(err) => Err(InternalError::from_response("", err).into()),
+    }
+}
+
+pub async fn login(
+    credentials: web::Json<Credentials>,
+    session: Session,
+) -> Result<impl Responder, Error> {
+    let credentials = credentials.into_inner();
+
+    match User::authenticate(credentials) {
+        Ok(user) => session.insert("user_id", user.id).unwrap(),
+        Err(err) => return Err(InternalError::from_response("", err).into()),
+    };
+
+    Ok("Welcome!")
+}
+
+async fn secret(session: Session) -> Result<impl Responder, Error> {
+    validate_session(&session).map_err(|err| InternalError::from_response("", err))?;
+
+    Ok("secret revealed")
+}
+
+pub fn validate_session(session: &Session) -> Result<i64, HttpResponse> {
+    let user_id: Option<i64> = session.get("user_id").unwrap_or(None);
+
+    match user_id {
+        Some(id) => {
+            session.renew();
+            Ok(id)
+        }
+        None => Err(HttpResponse::Unauthorized().json("Unauthorized")),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use actix_web::{middleware, web, App, HttpServer};
+use actix_session::{SessionMiddleware, storage::RedisSessionStore};
 use dotenv::dotenv;
 use env_logger::Env;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use actix_web::cookie::Key;
 use webbrowser;
 mod steam;
 mod gog;
 mod games;
+mod authentication;
 mod epic;
 mod error;
 use error::MyError;
@@ -21,10 +24,15 @@ async fn main() -> std::io::Result<()> {
         steam_id: Mutex::new(None),
     });
 
+    let secret_key = Key::generate();
+
+    let redis_store = RedisSessionStore::new("redis://127.0.0.1:6379").await.unwrap();
+
     let server = HttpServer::new(move || {
         let shared_state_clone = shared_state.clone();
         App::new()
             .wrap(middleware::Logger::default())
+            .wrap(SessionMiddleware::new(redis_store.clone(), secret_key.clone()))
             .app_data(web::Data::new(shared_state_clone))
             .service(
                 web::scope("/auth/steam")
@@ -34,22 +42,25 @@ async fn main() -> std::io::Result<()> {
             .service(
                 web::scope("/auth/gog")
                     .route("/login", web::get().to(gog::login::login))
-                    .route("/code_temp", web::get().to(gog::login::handle_code_temp)), // Add this line
+                    .route("/code_temp", web::get().to(gog::login::handle_code_temp)),
             )
             .service(
                 web::scope("/auth/epic")
                     .route("/login", web::get().to(epic::login::login))
-                    .route("/code_temp", web::get().to(epic::login::handle_code_temp)), // Add this line
+                    .route("/code_temp", web::get().to(epic::login::handle_code_temp)),
             )
             .route("/check_steam_id", web::get().to(steam::login::check_steam_id))
+            .service(
+                web::scope("/users")
+                    .route("/register", web::post().to(authentication::auth::register))
+                    .route("/login", web::post().to(authentication::auth::login)),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run();
 
-    // Open the web browser to the login page after the server has started
     let url = "http://127.0.0.1:8080/auth/epic/login";
     thread::spawn(move || {
-        // Add a small delay to ensure the server has started
         thread::sleep(std::time::Duration::from_secs(1));
         if webbrowser::open(url).is_err() {
             eprintln!("Failed to open browser. Please navigate to {}", url);
@@ -58,4 +69,3 @@ async fn main() -> std::io::Result<()> {
 
     server.await
 }
-
